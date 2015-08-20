95893d926f93e4eca0c0870cf3d5e8d47881fd00
Jared Hirsch <ohai@6a68.net>
2015-08-19 17:04:23 -0700

    Replace var with let or const.

--- code/bootstrap.js
+++ code/bootstrap.js
@@ -17,22 +17,21 @@ XPCOMUtils.defineLazyModuleGetter(this, "Services",
  */
 
 // if true, abort any remaining DB requests or beacons
-var isExiting = false;
+let isExiting = false;
 
 // see https://bugzil.la/1174937#c16 for explanation of query optimizations
-var query = "SELECT SUM(visit_count) AS count, url FROM moz_places " +
-                     "WHERE rev_host BETWEEN :reversed AND :reversed || X'FFFF' " +
-                     "AND url LIKE :fuzzy";
+const query = `SELECT SUM(visit_count) AS count, url FROM moz_places
+               WHERE rev_host BETWEEN :reversed AND :reversed || X'FFFF'
+               AND url LIKE :fuzzy`;
 
 // we need a window pointer to get access to navigator.sendBeacon, but we have
 // to wait until a DOMWindow is ready (see runExperiment below)
-var window;
+let window;
 
-// XXX update these URLs for beta vs release populations
 const countUrl = 'https://statsd-bridge.services.mozilla.com/count/beta39.1174937.serpfraction.';
 const gaugeUrl = 'https://statsd-bridge.services.mozilla.com/gauge/beta39.1174937.serpfraction.';
 
-var searchProviders = {
+const searchProviders = {
   google: {
     reversed: 'moc.elgoog.',
     fuzzy: '%google.com/search?q%'
@@ -51,7 +50,7 @@ var searchProviders = {
   }
 };
 
-var counts = {
+const counts = {
   google: null,
   yahoo: null,
   bing: null,
@@ -59,31 +58,31 @@ var counts = {
   total: null
 };
 
-var saveCount = function(providerName, results) {
+function saveCount(providerName, results) {
   // query returns undefined if there are no visits to the specified page; replace with 0
-  var count = results && results[0] && results[0].getResultByName('count') || 0;
+  let count = results && results[0] && results[0].getResultByName('count') || 0;
   if (Number.isInteger(count)) {
     counts[providerName] = count;
     return Promise.resolve(count);
   } else {
     return Promise.reject(new Error('count for ' + providerName + ' was not an integer: ' + count));
   }
-};
+}
 
-var getTotalCount = function(db) {
+function getTotalCount(db) {
   if (isExiting) { return Promise.reject(new Error('aborting because isExiting is true')); }
-  var totalQuery = 'SELECT COUNT(*) AS count FROM moz_historyvisits;';
+  const totalQuery = 'SELECT COUNT(*) AS count FROM moz_historyvisits;';
   return db.execute(totalQuery);
-};
+}
 
 // returns an integer percentage or null if either operand was invalid
 // division operator handles type coercion for us
-var percentage = function(a, b) {
-  var result = a/b;
+function percentage(a, b) {
+  const result = a/b;
   return isFinite(result) ? Math.round(result * 100) : null;
-};
+}
 
-var sendBeacon = function(url, data) {
+function sendBeacon(url, data) {
   if (isExiting) { return; }
   try {
     window.navigator.sendBeacon(url, data);
@@ -91,12 +90,12 @@ var sendBeacon = function(url, data) {
     // something's wrong, give up
     uninstall();
   }
-};
+}
 
 // For each search provider, either send the result percentage for that
 // provider, or increment an error counter. Also send down the total history
 // size for that user, and increment the total count of responding clients.
-var send = function(data) {
+function send(data) {
   ['google', 'yahoo', 'bing', 'amazon'].forEach(function(provider) {
     let pct = percentage(counts[provider], counts.total);
     if (pct !== null) {
@@ -112,7 +111,7 @@ var send = function(data) {
 // If an error occurs when querying or connecting to the DB, just give up:
 // fire a beacon with the name of the failed step (in dot-delimited statsd
 // format) and uninstall the experiment.
-var onError = function(step, err) {
+function onError(step, err) {
   sendBeacon(countUrl + 'error.' + step, 1)
   uninstall();
 }
@@ -120,36 +119,36 @@ var onError = function(step, err) {
 // annoying, but unavoidable, window management code
 // implements nsIWindowMediatorListener
 // pulled from MDN: https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XPCOM/Reference/Interface/nsIWindowMediator#addListener()
-var windowListener = {
+let windowListener = {
   onOpenWindow: function(aWindow) {
     Services.wm.removeListener(windowListener);
     let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).
                             getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-    var onDomWindowReady = function() {
+    function onDomWindowReady() {
       domWindow.removeEventListener('load', onDomWindowReady);
       // assign the addon-global window variable, so that
       // "window.navigator.sendBeacon" will be defined
       window = domWindow;
       _runExperiment();
-    };
+    }
     domWindow.addEventListener('load', onDomWindowReady);
   },
   onCloseWindow: function(aWindow) {},
   onWindowTitleChange: function(aWindow, aTitle) {}
 };
 
-var runExperiment = function() {
+function runExperiment() {
   if (isExiting) { return; }
   // get a window, or wait till a window is opened, then continue.
-  var win = Services.wm.getMostRecentWindow('navigator:browser');
+  let win = Services.wm.getMostRecentWindow('navigator:browser');
   if (win) {
     windowListener.onOpenWindow(win);
   } else {
     Services.wm.addListener(windowListener);
   }
-};
+}
 
-var _runExperiment = function() {
+function _runExperiment() {
   if (isExiting) { return; }
   return PlacesUtils.promiseDBConnection()
     // google bits
@@ -183,12 +182,12 @@ var _runExperiment = function() {
     .then(send, onError.bind(null, 'send'))
     // when finished, uninstall yourself
     .then(uninstall, onError.bind(null, 'uninstall'));
-};
+}
 
-var exit = function() {
+function exit() {
   // abort any future Places queries or beacons
   isExiting = true;
-};
+}
 
 /*
  *  bootstrapped addon code
